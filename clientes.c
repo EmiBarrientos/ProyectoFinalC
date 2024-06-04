587863a0ed5596bfdb23f483b5626af3edf2c06c
Emiliano <86033466+EmiBarrientos@users.noreply.github.com>
2024-06-04 19:33:51 -0300

    subiendo librerias de cliente

---
+++ clientes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "estructuras.h"
+#include "domicilios.h"
+
+
+
+
+
+
+
+
---
+++ clientes.h
@@ -0,0 +1,17 @@
+#ifndef CLIENTES_H_INCLUDED
+#define CLIENTES_H_INCLUDED
+
+typedef struct{
+    int id; /// campo único y autoincremental
+    int nroCliente;
+    char nombre[30];
+    char apellido[30];
+    char dni[10];
+    char email[30];
+    stDomicilio domicilio;
+    char telefono[12];
+    int eliminado;
+}stCliente
+
+
+#endif // CLIENTES_H_INCLUDED
