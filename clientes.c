283e9ca3b01a8cffbea0e6879a93060ae31a5305
Emiliano <86033466+EmiBarrientos@users.noreply.github.com>
2024-06-04 20:49:53 -0300

    Update clientes.c
    codigo no terminado

--- clientes.c
+++ clientes.c
@@ -5,6 +5,56 @@
 
 
 
+stCliente cargaUnCliente(){
+
+    stCliente cliente;
+    char palabra[40];
+
+
+    printf("ingrese el nombre del cliente: \n");
+    fflush(stdin);
+    gets(palabra);
+    while(validarNombre(palabra)){
+        printf("datos invalidos, intente nuevamente\n")
+        system("cls");
+        printf("ingrese el nombre del cliente: \n");
+        fflush(stdin);
+        gets(palabra);
+    }
+        cliente.nombre=palabra;
+
+
+
+    printf("ingrese el apellido del cliente: \n");
+    fflush(stdin);
+    gets(cliente.apellido);
+    printf("ingrese el DNI del cliente: \n");
+    fflush(stdin);
+    gets(cliente.dni);
+    printf("ingrese el Email del cliente: \n");
+    fflush(stdin);
+    gets(cliente.email);
+
+
+
+
+return cliente;
+}
+
+
+int validarNombre(char palabra[], int v){
+    palabraValida=0;
+    for(int i=0; i<v;i++){
+        if(isdigit(palabra[i]))
+
+    }
+
+
+return palabraValida;
+}
+
+
+
 
 
 
